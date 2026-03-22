eeb60115768bfd1069008cc9dfc8cca1b08bc59e
Eskil Uhlving Larsen <7443949+picccard@users.noreply.github.com>
2026-03-22 02:05:39 +0100

    add brew completions in pwsh

--- home/dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,9 @@
+if ((Get-Command brew) -and (Test-Path ($completions = "$(brew --prefix)/share/pwsh/completions"))) {
+  foreach ($f in Get-ChildItem -Path $completions -File) {
+    . $f
+  }
+}
+
 # oh-my-posh init pwsh --config /opt/homebrew/opt/oh-my-posh/themes/jtracey93.omp.json | Invoke-Expression
 
 $ENV:STARSHIP_CONFIG = "$HOME/.config/starship/starship.toml"
