518a50574e5f72f24624e7ee7f4d551cf9386e4d
Eskil Uhlving Larsen <7443949+picccard@users.noreply.github.com>
2026-03-22 02:04:23 +0100

    add starship shell prompt

--- home/dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,4 @@
-oh-my-posh init pwsh --config /opt/homebrew/opt/oh-my-posh/themes/jtracey93.omp.json | Invoke-Expression
+# oh-my-posh init pwsh --config /opt/homebrew/opt/oh-my-posh/themes/jtracey93.omp.json | Invoke-Expression
+
+$ENV:STARSHIP_CONFIG = "$HOME/.config/starship/starship.toml"
+Invoke-Expression (&starship init powershell)
