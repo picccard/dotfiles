59e1aa61b2adbdfb418050863c4db1ca659a0262
Eskil Uhlving Larsen <7443949+picccard@users.noreply.github.com>
2026-03-22 02:22:03 +0100

    add mise to handle kubectl, helm, kustomize and opentofu

--- home/dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,8 @@ if ((Get-Command brew) -and (Test-Path ($completions = "$(brew --prefix)/share/p
   }
 }
 
+(&mise activate pwsh) | Out-String | Invoke-Expression
+
 # oh-my-posh init pwsh --config /opt/homebrew/opt/oh-my-posh/themes/jtracey93.omp.json | Invoke-Expression
 
 $ENV:STARSHIP_CONFIG = "$HOME/.config/starship/starship.toml"
